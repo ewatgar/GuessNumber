ecfa5320a28832d8bdfddc8144bba95b8431224e
Ender Watts <103525727+ewatgar@users.noreply.github.com>
2023-12-23 10:41:09 +0100

    interfaz ConfigActivity

---
+++ app/src/main/java/com/example/guessnumber/EndPlayActivity.kt
@@ -0,0 +1,11 @@
+package com.example.guessnumber
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EndPlayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_end_play)
+    }
+}
